27646dc5b56d43283f7179580cb3191fef4adf12
remiliang <remiliang@synology.com>
2018-06-21 14:54:07 +0800

    2018-06-21 update, add webhook class and readme

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # line-bot-for-wgtw
 Line bot webhook for worldgym Taiwan
+
+## 查課表
+提供**今天**的有氧課程
+* 查課表 *店家*
+    * 店家為 worldgym Taiwan 官網上的店家名稱
+    * e.g
+    	1. 101
+    	2. 西門
+	    3. 站前
+	    4. 板橋府中
+	    5. 台中sogo
+	    6. 高雄sogo
+	
+* 查課表 *課程*
+    * 課程為 worldgym Taiwan 官網上的課程名程以及其 alias 
+    * e.g 
+	    1. "強力瑜珈"              => "power yoga"
+	    2. "階梯有氧 I"            => "step move i" 
+	    3. "戰鬥有氧(Body Combat)" => "body combat"
+    * 提供以下課程有可輸入縮寫來查詢課程
+	    1. 戰鬥有氧(Body Combat)    => bc
+	    2. 身心靈平衡(Body Balance) => bb
+	    3. 曲線槓鈴(Body Pump)      => bp
+	    4. 搖擺炫舞(Body Jam)       => bj
+	    5. 希巴姆舞蹈(SH'BAM)       => sb
+
+## 查公告
+查詢該分店**最新**的公告
+* 查公告 *店家*
+    * worldgym Taiwan 官網上的店家名稱
---
+++ webhook.php
@@ -0,0 +1,224 @@
+<?php
+
+class webhook
+{
+	protected $input;
+	protected $inputMessage;
+	protected $inputArgument;
+	protected $replyToken;
+
+	protected $noClassMessage = '沒有這堂課耶，你要來當老師嗎 ';
+	protected $noOfficeMessage = '沒有這家店耶，你要來當開一家嗎 ';
+
+	protected $lineAPI = 'https://api.line.me/v2/bot/message/reply';
+	protected $wgPostAPI = 'http://www.worldgymtaiwan.com/api/post';
+	protected static $curl = null;
+
+	protected $accessToken = 'LINE_ACCESS_TOKEN';
+
+	private function getEmoji($code) {
+		$bin = hex2bin(str_repeat('0', 8 - strlen($code)) . $code);
+		return mb_convert_encoding($bin, 'UTF-8', 'UTF-32BE');
+	}
+
+	private function getFile($fileName) {
+		return json_decode(file_get_contents($fileName), true);
+	}
+
+	private function jsonResponse($message = [])
+	{
+		header("Content-type: application/json; charset=utf-8");
+		echo json_encode($message);
+		exit;
+	}
+
+	private function parseLineInput()
+	{
+		if (empty($this->input)) {
+			$this->jsonResponce([
+				'success' => true,
+				'message' => 'no paramters'
+			]);
+		}
+
+		if (empty($this->input['events']) || empty($this->input['events'][0])) {
+			$this->jsonResponce([
+				'success' => true,
+				'message' => 'empty event'
+			]);
+		}
+
+		$event = $this->input['events'][0];
+		if (empty($event['message'])) {
+			$this->jsonResponce([
+				'success' => true,
+				'message' => 'empty user message'
+			]);
+		}
+
+		$userMessageInfo = $event['message'];
+		$this->messagetype = $userMessageInfo['type'];
+		$this->inputMessage = $userMessageInfo['text'];
+		$this->replyToken = $event['replyToken'];
+
+		if ('text' != $this->messagetype) {
+			$this->jsonResponce([
+				'success' => true,
+				'message' => 'not text message'
+			]);
+		}
+	}
+
+	private function getTodayClasses($inputClasses, $type = 'course')
+	{
+		$classes = [];
+
+		date_default_timezone_set('Asia/Taipei');
+		$now = date('H:i:s');
+		$day = date('N');
+
+		if ( ! empty($inputClasses[$day])) {
+			$wantedOfficeClasses = $inputClasses[$day];
+
+			foreach ($wantedOfficeClasses as $time => $infos) {
+
+				// if ($time > $now) {
+				foreach ($infos as $info) {
+
+					if ('course' == $type) {
+						$temp = [$info['office'], $time, $info['teacher']];
+					} else {
+						$temp = [$time, $info['teacher'], $info['course']];
+					}
+
+					$nextClasses[] = implode('  ', $temp);
+				}
+				// }
+			}
+		}
+
+		return $nextClasses;
+	}
+
+	private function getClasses()
+	{
+		$nextClasses = [];
+
+		$courseAliasList = $this->getFile('line_course_alias_list.json');
+
+		if ( ! empty($courseAliasList[$this->inputArgument])) {
+			$this->inputArgument = $courseAliasList[$this->inputArgument];
+		}
+
+		$courseList = $this->getFile('line_course_list.json');
+		$officeList = $this->getFile('line_office_list.json');
+
+		if ( ! empty($courseList[$this->inputArgument])) {
+			$nextClasses = $this->getTodayClasses($courseList[$this->inputArgument]);
+		} else if ( ! empty($officeList[$this->inputArgument])) {
+			$nextClasses = $this->getTodayClasses($officeList[$this->inputArgument], 'office');
+		}
+
+		if (empty($nextClasses)) {
+			$nextClasses[] = $this->noClassMessage. $this->getEmoji('100078');
+		}
+
+		return implode("\n", $nextClasses);
+	}
+
+	private function getLatestAnnounce()
+	{
+		$officeMap = $this->getFile('line_office_map.json');
+
+		if (empty($officeMap[$this->inputArgument])) {
+			return $this->noOfficeMessage. getEmoji('100078');
+		}
+
+		$apiParams = [
+			'office_id' => $officeMap[$this->inputArgument],
+			'sort' => '-released_at',
+		];
+		$announceURL = $this->wgPostAPI.'?'.http_build_query($apiParams);
+
+		$res = json_decode(file_get_contents($announceURL), true);
+		if ( ! empty($res) && ! empty($res['data'])) {
+
+			$data = $res['data'][0];
+
+			if ( ! empty($data['details'])) {
+				$message = str_replace('&nbsp;', "\n", $data['details']);
+				$message = str_replace('</div><div>', "\n", $message);
+				$message = strip_tags($message);
+			}
+		}
+
+		return $message;
+	}
+
+	private function handleUserMessage()
+	{
+		if (preg_match('/^查課表\s+(.*)$/', $this->inputMessage, $matches)) {
+			$this->inputArgument = strtolower($matches[1]);
+			$responseMessage = $this->getClasses();
+		}
+
+		if (preg_match('/^查公告\s+(.*)$/', $this->inputMessage, $matches)) {
+			$this->inputArgument = strtolower($matches[1]);
+			$responseMessage = $this->getLatestAnnounce();
+		}
+
+		$response = [
+			"replyToken" => $this->replyToken,
+			"messages" => [
+				[
+					"type" => "text",
+					"text" => $responseMessage
+				]
+			]
+		];
+
+		if ( ! empty($this->replyToken)) {
+			$this->sendMessagetoLine($response);
+			exit;
+		} else {
+			$this->jsonResponse($response);
+		}
+
+	}
+
+	private function sendMessagetoLine($postData = array())
+	{
+		curl_setopt_array(static::$curl, array(
+			CURLOPT_POST            => true,
+			CURLOPT_CUSTOMREQUEST   => 'POST',
+			CURLOPT_CONNECTTIMEOUT  => 30,
+			CURLOPT_RETURNTRANSFER  => true,
+			CURLOPT_TIMEOUT         => 30,
+			CURLOPT_SSL_VERIFYHOST  => 0,
+			CURLOPT_POSTFIELDS      => json_encode($postData),
+			CURLOPT_HTTPHEADER      => [
+				'Content-Type: application/json',
+				'Authorization: Bearer '.$this->accessToken
+			]
+		));
+
+		$result = curl_exec(static::$curl);
+		curl_close(static::$curl);
+	}
+
+	function __construct ()
+	{
+		if (empty($_GET)) {
+			$this->input = json_decode(file_get_contents('php://input'), true);
+			$this->parseLineInput();
+		} else {
+			$this->inputMessage = $this->input = $_GET['test'];
+		}
+
+		static::$curl = curl_init($this->lineAPI);
+
+		$this->handleUserMessage();
+	}
+}
+
+new webhook();
